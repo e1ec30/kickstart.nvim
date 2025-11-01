d8401e859595242b59e938f31b7ac0d0a289fa7d
e1ec30 <thelastelectro@gmail.com>
2025-11-01 14:21:17 +0100

    Updates

--- init.lua
+++ init.lua
@@ -199,6 +199,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Global tabstop and co settings
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
 -- Load Custom Keymaps
 require 'custom.keymaps'()
 
